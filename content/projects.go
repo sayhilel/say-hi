e01937758fb9449c0508164fc217cfae22fd84ea
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-05-25 02:10:54 +0530

    fuck webdev

---
+++ content/projects.go
@@ -0,0 +1,7 @@
+package content
+
+type Project struct {
+	Name        string
+	Url         string
+	Description string
+}
--- handlers/landing.go
+++ handlers/landing.go
@@ -1,17 +1,26 @@
 package handlers
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/sayhilel/say-hi/content"
 )
 
 func LandingHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+	return c.Render("index", fiber.Map{
+		"Name": "PJ1",
+	})
 }
 
-func SecretHandler(c *fiber.Ctx) error {
-	fmt.Println("Secret")
-	return c.SendString("Nice")
+func ProjectsHandler(c *fiber.Ctx) error {
+	p := content.Project{Name: "PJ1", Url: "PJ1.com", Description: "It does it all"}
 
+	return c.Render("index", fiber.Map{
+		"Name": p.Name + " -> " + p.Description + " -> " + p.Url,
+	})
+}
+
+func CollapseHandler(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{
+		"Name": "PJ1",
+	})
 }
