741793f3c7660e9d608283ba916e00f618063e14
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-07-01 16:20:28 +0530

    cleaned nav

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,39 +1,39 @@
-function returnHome(event) {
-    if (event.ctrlKey && event.key === "c") {
-        htmx.ajax('GET', "about-me", '#terminal-window');
-    }
-}
-
-function initializeNav() {
-    let opt = document.querySelectorAll(".selector li");
+function listenNav() {
     let index = 0;
-    opt[index].classList.add("highlight");
-
-    document.addEventListener("keydown", navHandle);
-
-    function navHandle(event) {
-        const projectsDiv = document.getElementById("projects");
-        if (!projectsDiv) return;
-
-        const rect = projectsDiv.getBoundingClientRect();
-        const isVisible = rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
-
-        if (isVisible) {
-            const url = `/projects/${index}`;
-            switch (event.key) {
-                case "ArrowUp":
-                    opt[index].classList.remove("highlight");
-                    index = (index - 1 + opt.length) % opt.length;
-                    opt[index].classList.add("highlight");
-                    htmx.ajax('GET', url, '#project-box');
-                    break;
-                case "ArrowDown":
-                    opt[index].classList.remove("highlight");
-                    index = (index + 1) % opt.length;
-                    opt[index].classList.add("highlight");
-                    htmx.ajax('GET', url, '#project-box');
-                    break;
+    document.addEventListener("keydown",
+        function(event) {
+            // Global
+            if (event.ctrlKey && event.key === "c") {
+                htmx.ajax('GET', "about-me", '#terminal-window');
             }
-        }
-    }
+
+            // Prompt
+            let prompt = document.getElementById('command');
+            if (prompt) {
+                prompt.focus()
+            }
+
+            // Projects
+            let projects = document.getElementById("projects");
+            if (projects) {
+                let opt = document.querySelectorAll(".selector li");
+
+                const url = `/projects/${index}`;
+                switch (event.key) {
+                    case "ArrowUp":
+                        opt[index].classList.remove("highlight");
+                        index = (index - 1 + opt.length) % opt.length;
+                        opt[index].classList.add("highlight");
+                        htmx.ajax('GET', url, '#project-box');
+                        break;
+                    case "ArrowDown":
+                        opt[index].classList.remove("highlight");
+                        index = (index + 1) % opt.length;
+                        opt[index].classList.add("highlight");
+                        htmx.ajax('GET', url, '#project-box');
+                        break;
+                }
+            }
+
+        });
 }
